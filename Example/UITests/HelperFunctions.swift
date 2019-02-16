a26dcff1b615c091677e6b8b1a89d37c475997ee
Stephan Lerner <stephanlerner@icloud.com>
2019-02-16 19:07:36 +0100

    added uitest

---
+++ Example/UITests/HelperFunctions.swift
@@ -0,0 +1,16 @@
+//  Copyright © 2019 Stephan Lerner. All rights reserved.
+
+import XCTest
+
+class UITests: XCTestCase {
+    
+    func testRootNodeHandlingFor(ressourceIdentifier: String, elementTreeValidationHandler: (_ elementTree: [XCUIElement]) -> Void) {
+        let app = XCUIApplication()
+        app.launchArguments.append("-ressourceIdentifier")
+        app.launchArguments.append(ressourceIdentifier)
+        app.launch()
+        sleep(60)
+        elementTreeValidationHandler(app.groups["SKView"].children(matching: .image).allElementsBoundByIndex)
+    }
+
+}
---
+++ Example/UITests/UITests.swift
@@ -0,0 +1,25 @@
+//  Copyright © 2019 Stephan Lerner. All rights reserved.
+
+import XCTest
+
+extension UITests {
+
+    func testMacOSProject() {
+        testRootNodeHandlingFor(ressourceIdentifier: MacOSProject.identifier()) { (elementTree) in
+            for element in elementTree {
+                print(element.label)
+                print(element.frame)
+            }
+        }
+    }
+    
+    func testIOSProject() {
+        testRootNodeHandlingFor(ressourceIdentifier: IOSProject.identifier()) { (elementTree) in
+            for element in elementTree {
+                print(element.label)
+                print(element.frame)
+            }
+        }
+    }
+
+}
