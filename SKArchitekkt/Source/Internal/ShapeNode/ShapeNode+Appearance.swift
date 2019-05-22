04b071dd6cb7c7f51093a5b81e777726a530d199
Stephan Lerner <stephanlerner@icloud.com>
2019-05-22 17:31:55 +0200

    commit workinfg state

--- SKArchitekkt/Source/Internal/ShapeNode/ShapeNode+Appearance.swift
+++ SKArchitekkt/Source/Internal/ShapeNode/ShapeNode+Appearance.swift
@@ -1,40 +1,3 @@
-//  Copyright © 2019 Stephan Lerner. All rights reserved.
-
-import SpriteKit
-import CoreArchitekkt
-
-extension ShapeNode {
-
-    // MARK: - Internal -
-
-    private static let radiusObjectAssociation = ObjectAssociation<CGFloat>()
-    private(set) var radius: CGFloat {
-        get { return ShapeNode.radiusObjectAssociation[self] ?? 16 }
-        set { ShapeNode.radiusObjectAssociation[self] = newValue }
-    }
-
-    func setUpPhysicsAndAppearance() {
-        setUpPhysicsBody()
-        set(collapsed: !node.isRoot)
-    }
-
-    func didDoubleTap() {
-        set(collapsed: !isCollapsed)
-    }
-
-    func setColors(_ dictionary: [String: NSColor]) {
-        scopeColorDictionary = dictionary
-        updateColor()
-    }
-    
-    func set(collapsed: Bool) {
-        let collapsed = node.children.count > 0 ? collapsed : true
-        isCollapsed = collapsed
-        updateChildren()
-        updateRadius()
-        updateAncestorsRadius()
-        resetChildrenPosition()
-    }
 
     #warning("move to scene")
 //    func updatePhysicsWith(forceDecay: CGFloat, velocityDecay: CGFloat) {
@@ -54,135 +17,12 @@ extension ShapeNode {
 
     // MARK: - Private -
 
-    private static let isCollapsedObjectAssociation = ObjectAssociation<Bool>()
-    private var isCollapsed: Bool {
-        get { return ShapeNode.isCollapsedObjectAssociation[self] ?? true }
-        set { ShapeNode.isCollapsedObjectAssociation[self] = newValue }
-    }
-    private static let scopeColorDictionaryObjectAssociation = ObjectAssociation<[String: NSColor]>()
-    private var scopeColorDictionary: [String: NSColor] {
-        get { return ShapeNode.scopeColorDictionaryObjectAssociation[self] ?? [:] }
-        set { ShapeNode.scopeColorDictionaryObjectAssociation[self] = newValue }
-    }
 //    private static let arcNodeDictionaryObjectAssociation = ObjectAssociation<[ShapeNode: SKShapeNode]>()
 //    private var arcNodeDictionary: [ShapeNode: SKShapeNode] {
 //        get { return ShapeNode.arcNodeDictionaryObjectAssociation[self] ?? [:] }
 //        set { ShapeNode.arcNodeDictionaryObjectAssociation[self] = newValue }
 //    }
 
-    private func setUpPhysicsBody() {
-        let physicsBody = SKPhysicsBody(circleOfRadius: radius)
-        physicsBody.isDynamic = true
-        physicsBody.charge = 0
-        physicsBody.mass = radius^^2
-        physicsBody.linearDamping = 0
-        physicsBody.allowsRotation = false
-        physicsBody.collisionBitMask = 0
-        self.physicsBody = physicsBody
-    }
-    
-    private func updateChildren() {
-        if isCollapsed {
-            castedChildren.forEach { $0.removeFromParent() }
-            self.castedChildren = []
-        } else {
-            let castedChildren = node.children.map { (node) -> ShapeNode in
-                let shapeNode = ShapeNode(node: node)
-                addChild(shapeNode)
-                return shapeNode
-            }
-            self.castedChildren = castedChildren
-        }
-    }
-
-    private func updateRadius() {
-        let minimumRadius = CGFloat(16)
-        let areaOfChildren = isCollapsed ? minimumRadius : castedChildren.map { $0.radius^^2 }.reduce(0, +)
-        let radius = max(minimumRadius, 2.2*sqrt(areaOfChildren))
-        path = CGPath(ellipseIn: CGRect(x: -radius, y: -radius, width: 2*radius, height: 2*radius), transform: nil)
-        self.radius = radius
-        updateConstraints()
-        updatePhysicsBody()
-        updateTextNodes()
-        updateColor()
-    }
-
-    private func updateAncestorsRadius() {
-        allCastedAncestors.forEach { $0.updateRadius() }
-    }
-
-    private func updateConstraints() {
-        castedChildren.forEach {
-            $0.constraints = [SKConstraint.distance(SKRange(lowerLimit: 0, upperLimit: radius - $0.radius), to: self)]
-        }
-    }
-
-    private func updatePhysicsBody() {
-        physicsBody?.mass = radius^^2
-    }
-
-    private func updateTextNodes() {
-        guard !isHidden else { return }
-        let name = node.identifier?.components(separatedBy: ".").last ?? node.scope
-        children.filter { $0 is SKLabelNode }.forEach { $0.removeFromParent() }
-        let nameLength = CGFloat(name.count)
-        var lettersPerFullCircle = CGFloat(24)
-        let angleDifference = 2*CGFloat.pi/lettersPerFullCircle
-        var angle: CGFloat
-        let isSpiral: Bool
-        if nameLength > lettersPerFullCircle {
-            angle = CGFloat.pi
-            isSpiral = true
-        } else {
-            angle = CGFloat.pi/2 + ((nameLength-1)/2)*angleDifference
-            isSpiral = false
-        }
-        let fontSize = radius/3
-        let fontScalingFactor = CGFloat(1.25)
-        let resutlingFontSize = fontSize/fontScalingFactor
-        for (index, character) in name.enumerated() {
-            let resultingRadius = isSpiral ? (radius - (CGFloat(index)/lettersPerFullCircle*resutlingFontSize)) : radius
-            let x = cos(angle)*(resultingRadius - resutlingFontSize)
-            let y = sin(angle)*(resultingRadius - resutlingFontSize)
-            let labelNode = SKLabelNode(text: "\(character)")
-            labelNode.position = CGPoint(x: x, y: y)
-            labelNode.zRotation = angle - CGFloat.pi/2
-            labelNode.fontSize = fontSize
-            if isCollapsed {
-                labelNode.fontColor = .black
-                labelNode.fontName = NSFont.systemFont(ofSize: fontSize).fontName
-            } else {
-                labelNode.fontColor = NSColor.textColor.blended(withFraction: 0.3, of: .clear)
-                labelNode.fontName = NSFont.systemFont(ofSize: fontSize, weight: .light).fontName
-            }
-            addChild(labelNode)
-            let resultingAngleDifference = isSpiral ? angleDifference/(resultingRadius/radius) : angleDifference
-            lettersPerFullCircle = 2*CGFloat.pi/resultingAngleDifference
-            angle -= resultingAngleDifference
-            if resultingRadius < radius/2 {
-                labelNode.text = "…"
-                break
-            }
-        }
-    }
-
-    private func updateColor() {
-        fillColor = isCollapsed ? colorForScope() : .clear
-        strokeColor = colorForScope()
-        lineWidth = 3
-    }
-
-    private func colorForScope() -> NSColor {
-        return scopeColorDictionary[node.scope, default: .windowFrameColor]
-    }
-
-    private func resetChildrenPosition() {
-        castedChildren.forEach {
-            $0.position = CGPoint(x: CGFloat.random(in: -radius/2...radius/2), y: CGFloat.random(in: -radius/2...radius/2))
-        }
-    }
-
-    #warning("move to scene")
 //    private func updateRadialGravitationalForceOnChildrenWith(forceDecay: CGFloat) {
 //        guard !isCollapsed else { return }
 //        castedChildren.forEach {
@@ -264,4 +104,3 @@ extension ShapeNode {
 //        arcNode.path = path
 //    }
 
-}
--- SKArchitekkt/Source/Internal/ShapeNode/ShapeNode.swift
+++ SKArchitekkt/Source/Internal/ShapeNode/ShapeNode.swift
@@ -9,19 +9,10 @@ class ShapeNode: SKShapeNode {
 
     let node: Node
     
-    private(set) var resultingArcs: [Node] = []
-    var castedChildren: [ShapeNode] = []
+    private(set) var resultingArcs: Set<Node> = []
+    private(set) var castedChildren: [ShapeNode] = []
     private(set) var siblingPairs: [(ShapeNode, ShapeNode)] = []
-    
-    var allCastedAncestors: [ShapeNode] {
-        var allCastedAncestors: [ShapeNode] = []
-        var node = self
-        while let parent = node.parent as? ShapeNode {
-            allCastedAncestors.append(parent)
-            node = parent
-        }
-        return allCastedAncestors
-    }
+    private(set) var radius: CGFloat = 16
     
     init(node: CoreArchitekkt.Node) {
         self.node = node
@@ -31,6 +22,166 @@ class ShapeNode: SKShapeNode {
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func didDoubleTap() {
+        set(collapsed: !isCollapsed)
+    }
+    
+    // MARK - Private -
+    
+    private var isCollapsed = true
+    private var scopeColorDictionary: [String: NSColor] = [:]
+    private var allCastedAncestors: [ShapeNode] {
+        var allCastedAncestors: [ShapeNode] = []
+        var node = self
+        while let parent = node.parent as? ShapeNode {
+            allCastedAncestors.append(parent)
+            node = parent
+        }
+        return allCastedAncestors
+    }
+    
+    private func setUpPhysicsAndAppearance() {
+        setUpPhysicsBody()
+        set(collapsed: !node.isRoot, forceFully: true)
+    }
+    
+    private func setUpPhysicsBody() {
+        let physicsBody = SKPhysicsBody(circleOfRadius: radius)
+        physicsBody.isDynamic = true
+        physicsBody.charge = 0
+        physicsBody.mass = radius^^2
+        physicsBody.linearDamping = 0
+        physicsBody.allowsRotation = false
+        physicsBody.collisionBitMask = 0
+        self.physicsBody = physicsBody
+    }
+    
+    private func set(collapsed: Bool, forceFully: Bool = false) {
+        let collapsed = node.children.count > 0 ? collapsed : true
+        guard collapsed != isCollapsed || forceFully else { return }
+        isCollapsed = collapsed
+        updateChildren()
+        updateRadius()
+        updateAncestorsRadius()
+        resetChildrenPosition()
+    }
+    
+    private func updateChildren() {
+        if isCollapsed {
+            castedChildren.forEach { $0.removeFromParent() }
+            self.castedChildren = []
+            self.siblingPairs = []
+            self.resultingArcs = Set(node.arcs + node.allDescendants.flatMap { $0.arcs })
+        } else {
+            let castedChildren = node.children.map { (node) -> ShapeNode in
+                let shapeNode = ShapeNode(node: node)
+                addChild(shapeNode)
+                return shapeNode
+            }
+            self.castedChildren = castedChildren
+            var siblingPairs: [(ShapeNode, ShapeNode)] = []
+            for (index, first) in castedChildren[..<castedChildren.count].enumerated() {
+                for second in castedChildren[(index+1)...] {
+                    siblingPairs.append((first, second))
+                }
+            }
+            self.siblingPairs = siblingPairs
+            self.resultingArcs = Set(node.arcs)
+        }
+    }
+    
+    private func updateRadius() {
+        let minimumRadius = CGFloat(16)
+        let areaOfChildren = isCollapsed ? minimumRadius : castedChildren.map { $0.radius^^2 }.reduce(0, +)
+        let radius = max(minimumRadius, 2.2*sqrt(areaOfChildren))
+        path = CGPath(ellipseIn: CGRect(x: -radius, y: -radius, width: 2*radius, height: 2*radius), transform: nil)
+        self.radius = radius
+        updateConstraints()
+        updatePhysicsBody()
+        updateTextNodes()
+        updateColor()
+    }
+    
+    private func updateAncestorsRadius() {
+        allCastedAncestors.forEach { $0.updateRadius() }
+    }
+    
+    private func updateConstraints() {
+        castedChildren.forEach {
+            $0.constraints = [SKConstraint.distance(SKRange(lowerLimit: 0, upperLimit: radius - $0.radius), to: self)]
+        }
+    }
+    
+    private func updatePhysicsBody() {
+        physicsBody?.mass = radius^^2
+    }
+    
+    private func updateTextNodes() {
+        guard !isHidden else { return }
+        let name = node.identifier?.components(separatedBy: ".").last ?? node.scope
+        children.filter { $0 is SKLabelNode }.forEach { $0.removeFromParent() }
+        let nameLength = CGFloat(name.count)
+        var lettersPerFullCircle = CGFloat(24)
+        let angleDifference = 2*CGFloat.pi/lettersPerFullCircle
+        var angle: CGFloat
+        let isSpiral: Bool
+        if nameLength > lettersPerFullCircle {
+            angle = CGFloat.pi
+            isSpiral = true
+        } else {
+            angle = CGFloat.pi/2 + ((nameLength-1)/2)*angleDifference
+            isSpiral = false
+        }
+        let fontSize = radius/3
+        let fontScalingFactor = CGFloat(1.25)
+        let resutlingFontSize = fontSize/fontScalingFactor
+        for (index, character) in name.enumerated() {
+            let resultingRadius = isSpiral ? (radius - (CGFloat(index)/lettersPerFullCircle*resutlingFontSize)) : radius
+            let x = cos(angle)*(resultingRadius - resutlingFontSize)
+            let y = sin(angle)*(resultingRadius - resutlingFontSize)
+            let labelNode = SKLabelNode(text: "\(character)")
+            labelNode.position = CGPoint(x: x, y: y)
+            labelNode.zRotation = angle - CGFloat.pi/2
+            labelNode.fontSize = fontSize
+            if isCollapsed {
+                labelNode.fontColor = .black
+                labelNode.fontName = NSFont.systemFont(ofSize: fontSize).fontName
+            } else {
+                labelNode.fontColor = NSColor.textColor.blended(withFraction: 0.3, of: .clear)
+                labelNode.fontName = NSFont.systemFont(ofSize: fontSize, weight: .light).fontName
+            }
+            addChild(labelNode)
+            let resultingAngleDifference = isSpiral ? angleDifference/(resultingRadius/radius) : angleDifference
+            lettersPerFullCircle = 2*CGFloat.pi/resultingAngleDifference
+            angle -= resultingAngleDifference
+            if resultingRadius < radius/2 {
+                labelNode.text = "…"
+                break
+            }
+        }
+    }
+    
+    private func setColors(_ dictionary: [String: NSColor]) {
+        scopeColorDictionary = dictionary
+        updateColor()
+    }
+    
+    private func updateColor() {
+        fillColor = isCollapsed ? colorForScope() : .clear
+        strokeColor = colorForScope()
+        lineWidth = 3
+    }
+    
+    private func colorForScope() -> NSColor {
+        return scopeColorDictionary[node.scope, default: .windowFrameColor]
+    }
+    
+    private func resetChildrenPosition() {
+        castedChildren.forEach {
+            $0.position = CGPoint(x: CGFloat.random(in: -radius/2...radius/2), y: CGFloat.random(in: -radius/2...radius/2))
+        }
     }
 
 }
