82135ae1d8a8e020b3ed0e424b548b2ae8a2cfdc
Stephan Lerner <stephanlerner@icloud.com>
2019-05-22 11:39:31 +0200

    removed performance workarround

--- SKArchitekkt/Source/Internal/NodeViewController/NodeViewController+RootNodeHandler.swift
+++ SKArchitekkt/Source/Internal/NodeViewController/NodeViewController+RootNodeHandler.swift
@@ -6,11 +6,6 @@ import CoreArchitekkt
 extension NodeViewController: RootNodeHandler {
 
     func handle(rootNode: Node) {
-        #warning("remove workarround when performance issues are gone")
-        ([rootNode] + rootNode.allDescendants).forEach {
-            $0.set(children: $0.children.filter({ $0.scope != "unknown" }))
-            $0.set(arcs: $0.arcs.filter({ $0.scope != "unknown" }))
-        }
         nodeView.handle(rootNode: rootNode)
     }
 
