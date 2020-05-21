15f438c0ca37e6d60eaa4cee2c245a9c638b1ef3
Stephan Lerner <Stephanlerner@icloud.com>
2020-05-21 21:18:36 +0200

    added codable to VirtualTransformation

--- SKArchitekkt/Source/Internal/NodeScene/NodeView/NodeScene/Virtual/VirtualTransformation.swift
+++ SKArchitekkt/Source/Internal/NodeScene/NodeView/NodeScene/Virtual/VirtualTransformation.swift
@@ -2,9 +2,40 @@
 
 import Foundation
 
-enum VirtualTransformation: Hashable {
-
+enum VirtualTransformation: Hashable, Codable {
+    
     // MARK: - Internal -
 
     case unfold(id: UUID)
+    
+    enum CodingKeys: CodingKey {
+        case unfold
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let key = container.allKeys.first
+        
+        switch key {
+        case .unfold:
+            let uuid = try container.decode(UUID.self, forKey: .unfold)
+            self = .unfold(id: uuid)
+        case .none:
+            throw DecodingError.dataCorrupted(
+                DecodingError.Context(
+                    codingPath: container.codingPath,
+                    debugDescription: "Unabled to decode enum."
+                )
+            )
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        switch self {
+        case let .unfold(id: uuid):
+            try container.encode(uuid, forKey: .unfold)
+        }
+    }
 }
