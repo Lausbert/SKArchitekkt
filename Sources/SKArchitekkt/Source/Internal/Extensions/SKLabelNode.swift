b92005f78699a5705abe40c5641c186d2368258d
Stephan Lerner <Stephanlerner@icloud.com>
2020-10-25 18:36:06 +0100

    fixed font size crash

--- Sources/SKArchitekkt/Source/Internal/Extensions/SKLabelNode.swift
+++ Sources/SKArchitekkt/Source/Internal/Extensions/SKLabelNode.swift
@@ -5,21 +5,23 @@ import SpriteKit
 extension SKLabelNode {
     convenience init(text: String, fontNamed font: String, fontSize: CGFloat, color: NSColor, shadowColor: NSColor) {
         self.init(text: "")
+        let maximumFontSize = CGFloat(10000/text.count)
+        let resultingFontSize = min(fontSize, maximumFontSize)
         
         let shadowNode = SKLabelNode(fontNamed: font)
         shadowNode.text = text
         shadowNode.fontColor = shadowColor
-        shadowNode.fontSize = fontSize
+        shadowNode.fontSize = resultingFontSize
         shadowNode.horizontalAlignmentMode = .center
         shadowNode.verticalAlignmentMode = .center
-        let shadowOffset = 0.015 * fontSize
+        let shadowOffset = 0.015 * resultingFontSize
         shadowNode.position = CGPoint(x: shadowOffset, y: shadowOffset)
         self.addChild(shadowNode)
         
         let textNode = SKLabelNode(fontNamed: font)
         textNode.text = text
         textNode.fontColor = color
-        textNode.fontSize = fontSize
+        textNode.fontSize = resultingFontSize
         textNode.horizontalAlignmentMode = .center
         textNode.verticalAlignmentMode = .center
         self.addChild(textNode)
